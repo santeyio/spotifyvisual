4466a31d786766099f25591393950cedf554d770
Caleb Hayashida <santeyio@gmail.com>
2017-08-25 16:30:59 -0400

    init

---
+++ home/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context = {}
+    return render(request, 'home/index.html', context)
+
+def callback(request):
+    context = {}
+    return render(request, 'home/callback.html', context)
