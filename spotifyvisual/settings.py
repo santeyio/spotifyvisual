abcddb22539796274bd4704663d377e7f73d34ef
Caleb Hayashida <santeyio@gmail.com>
2017-08-26 00:51:13 -0400

    change to site media

--- spotifyvisual/settings.py
+++ spotifyvisual/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import db_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,12 +77,7 @@ WSGI_APPLICATION = 'spotifyvisual.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = db_settings.db_settings
 
 
 # Password validation
@@ -120,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/site_media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'site_media')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
