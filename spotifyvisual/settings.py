367df6742703a1dd2712fe05dae6e1dc2514a07a
Caleb Hayashida <santeyio@gmail.com>
2017-08-28 14:11:14 -0400

    add allowed_hosts

--- spotifyvisual/settings.py
+++ spotifyvisual/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'xek-q0ai!t$e3oflek$c@)n!4mg#d7qbfxx4lnu8q5!7viknp^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'spotifyvisual.calebhayashida.com']
 
 
 # Application definition
